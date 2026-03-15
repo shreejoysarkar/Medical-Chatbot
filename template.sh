67baedf8aa20c8079df93f5c97c2bb047b049a28
shreejoy <sarkarshreejoy30@gmail.com>
2026-03-15 06:03:04 +0530

    folder template setup

---
+++ template.sh
@@ -0,0 +1,17 @@
+## Creating the directory
+
+mkdir -p src
+mkdir -p research
+
+## Creating the files
+
+touch src/__init__.py
+touch src/helper.py
+touch src/setup.py
+touch src/app.py
+touch research/trails.ipynb
+touch requirements.txt
+
+
+
+echo " Directory and files are created"
