32cbb8ecebc2aef12c368ad1fc31b5024a1fcbd8
shreejoy <sarkarshreejoy30@gmail.com>
2026-03-15 06:16:10 +0530

    setup.py

---
+++ src/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    Project_name = "medical-chatbot",
+    version = '0.0.1',
+    author = "Shreejoy",
+    author_email = "sarkarshreejoy30@gmail.com",
+    packages = find_packages(),
+    install_requires = []
+
+)
